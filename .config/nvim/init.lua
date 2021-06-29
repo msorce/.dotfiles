444409989cf763e2e8976c267a3cce74e33773e2
msorce <msorce@wsgc.com>
2021-06-28 18:49:58 -0700

    default configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,6 +14,7 @@ require('lv-which-key')
 
 -- LSP
 require('lsp')
+
 -- TODO should I put this in the filetype files?
 if O.lang.java.active then require('lsp.java-ls') end
 if O.lang.clang.active then require('lsp.clangd') end
