ca4388def677a5ec1fcb878d11426e29f92b9331
msorce <msorce@users.noreply.github.com>
2022-06-02 10:45:49 -0700

    refactored nvim configs
    I rebuilt nvim configurations and cleaned up unnecessary plugins and settings

---
+++ .config/nvim/lua/configurations/plugins.lua
@@ -0,0 +1,101 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+return require('packer').startup(function(use)
+  -- My plugins here
+  -- Visual
+  -- Colorschemes
+  use 'folke/tokyonight.nvim'
+  use 'lunarvim/darkplus.nvim'
+  use "folke/which-key.nvim"
+  use "akinsho/bufferline.nvim"
+  use "moll/vim-bbye"
+  use "nvim-lualine/lualine.nvim"
+  use "lukas-reineke/indent-blankline.nvim"
+  use "mhinz/vim-startify"
+
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+  -- Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- Comment
+  use "terrortylor/nvim-comment"
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+
+  -- Auto Pairs
+  use "windwp/nvim-autopairs"
+  -- Nvim Tree
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+  }
+
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
+  -- Startup screen
+  -- use {
+  --   "startup-nvim/startup.nvim",
+  --   requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+  --   config = function()
+  --     require"startup".setup(require"startup")
+  --   end
+  -- }
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
